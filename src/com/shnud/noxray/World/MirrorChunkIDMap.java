d4fc7298632cbbd000537a683765d8a72936f747
shnud <andrewshand94@gmail.com>
2014-01-03 23:35:42 +0000

    Back to using a fixed size int array for chunk ID map - easier

--- src/com/shnud/noxray/World/MirrorChunkIDMap.java
+++ src/com/shnud/noxray/World/MirrorChunkIDMap.java
@@ -14,7 +14,20 @@ public class MirrorChunkIDMap {
      * Holds all of the room IDs that are stored in a chunk, which are accesed by index or 'key' stored in the chunk data.
      * Slot 0 is reserved for room 0, i.e. not a room, in which case the room hider will not try to hide that block.
      */
-    private ArrayList<Integer> _slots = Lists.newArrayList((Integer) 0);
+    private int[] _slots = new int[MAXIMUM_SLOTS + 1];
+
+    /**
+     * Return the first empty ID slot (ignores 0 as it's always empty)
+     * @return the first empty slot, -1 if no empty slot was found
+     */
+    private int firstEmpty() {
+        for(int i = 1; i < _slots.length; i++) {
+            if(_slots[i] == 0)
+                return i;
+        }
+
+        return -1;
+    }
 
     public boolean isEmpty() {
         for(int ID : _slots) {
@@ -26,75 +39,71 @@ public class MirrorChunkIDMap {
     }
 
     public boolean isFull() {
-        if(_slots.size() < MAXIMUM_SLOTS + 1)
+        if(_slots.length < MAXIMUM_SLOTS + 1)
             return false;
 
-        for(int i = 1; i < _slots.size(); i++) {
-            if(i == 0) return false;
-        }
-
-        return true;
+        if(firstEmpty() < 0)
+            return true;
+        else
+            return false;
     }
 
     public int getRoomIDForKey(int key) {
-        return _slots.get(key);
+        return _slots[key];
+    }
+
+    public int indexOf(int roomID) {
+        for(int i = 0; i < _slots.length; i++) {
+            if(_slots[i] == roomID)
+                return i;
+        }
+
+        return -1;
     }
 
     public int getKeyForRoomID(int roomID) {
-        return _slots.indexOf(roomID);
+        return indexOf(roomID);
     }
 
     public void setSlotToID(int slot, int roomID) {
         if(slot == 0)
             throw new IllegalArgumentException("Cannot add a room ID to slot 0 as it is reserved for 'not a room' status");
 
-        if(slot >= _slots.size())
-            _slots.ensureCapacity(slot + 1);
-
-        _slots.set(slot, roomID);
+        _slots[slot] = roomID;
     }
 
-    public int getAmountOfSlots() {
-        /*
-         * Here we take away 1 because 0 is never used as a proper room ID slot and is reserved for "not a room", i.e. 0
-         */
-        return _slots.size() - 1;
+    public int[] getSlots() {
+        return _slots;
     }
 
     public boolean containsRoomID(int roomID) {
-        return getKeyForRoomID(roomID) != 0;
+        return getKeyForRoomID(roomID) > 0;
     }
 
-    public int addRoomID(int roomID) throws ChunkIDSlotsFullException {
-        int slot = _slots.indexOf(roomID);
+    public int addRoomID(int roomID) {
+        int slot = indexOf(roomID);
 
         if(slot > 0)
             return slot;
 
-        int firstEmpty = -1;
-
-        for(int i = 1; i < _slots.size(); i++) {
-            if (_slots.get(i) == 0)
-                firstEmpty = i;
-        }
+        int firstEmpty = firstEmpty();
 
         if(firstEmpty > 0) {
-            _slots.set(firstEmpty, roomID);
+            _slots[firstEmpty] = roomID;
             return firstEmpty;
         }
-        else {
-            if(_slots.size() < MAXIMUM_SLOTS) {
-                _slots.add(roomID);
-                return _slots.size() - 1;
-            }
-            else
-                throw new ChunkIDSlotsFullException();
-        }
+        else
+            return -1;
     }
 
     public void removeRoomID(int roomID) {
-        if(roomID != 0)
-            _slots.remove(roomID);
+        if(roomID == 0)
+            return;
+
+        for(int i = 1; i < _slots.length; i++) {
+            if(_slots[i] == roomID)
+                _slots[i] = 0;
+        }
     }
 
     public class ChunkIDSlotsFullException extends Exception {}
