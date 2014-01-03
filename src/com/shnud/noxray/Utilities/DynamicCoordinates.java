78902f5adbb1ddba1eb0e04d2e8ddd39cb1987a3
shnud <andrewshand94@gmail.com>
2014-01-03 23:32:46 +0000

    Fix unnecessary conversion of Y values, added relative coordinate functions

--- src/com/shnud/noxray/Utilities/DynamicCoordinates.java
+++ src/com/shnud/noxray/Utilities/DynamicCoordinates.java
@@ -22,7 +22,7 @@ public class DynamicCoordinates {
     public static DynamicCoordinates initWithChunkCoordinates(int chunkX, int chunkY, int chunkZ) {
         return new DynamicCoordinates(
                 chunkX * MagicValues.HORIZONTAL_BLOCKS_IN_CHUNK,
-                chunkY * MagicValues.HORIZONTAL_BLOCKS_IN_CHUNK,
+                chunkY,
                 chunkZ * MagicValues.HORIZONTAL_BLOCKS_IN_CHUNK,
                 PrecisionLevel.CHUNK
         );
@@ -31,7 +31,7 @@ public class DynamicCoordinates {
     public static DynamicCoordinates initWithRegionCoordinates(int regionX, int regionY, int regionZ) {
         return new DynamicCoordinates(
                 regionX * MagicValues.HORIZONTAL_CHUNKS_IN_REGION * MagicValues.HORIZONTAL_BLOCKS_IN_CHUNK,
-                regionY * MagicValues.HORIZONTAL_CHUNKS_IN_REGION * MagicValues.HORIZONTAL_BLOCKS_IN_CHUNK,
+                regionY,
                 regionZ * MagicValues.HORIZONTAL_CHUNKS_IN_REGION * MagicValues.HORIZONTAL_BLOCKS_IN_CHUNK,
                 PrecisionLevel.REGION
         );
@@ -45,11 +45,18 @@ public class DynamicCoordinates {
     public int blockY() {return _blockY;}
     public int blockZ() {return _blockZ;}
     public int chunkX() {return _blockX >> MagicValues.BITSHIFTS_RIGHT_BLOCK_TO_CHUNK;}
-    public int chunkY() {return _blockY >> MagicValues.BITSHIFTS_RIGHT_BLOCK_TO_CHUNK;}
+    public int chunkY() {return _blockY;}
     public int chunkZ() {return _blockZ >> MagicValues.BITSHIFTS_RIGHT_BLOCK_TO_CHUNK;}
     public int regionX() {return _blockX >> MagicValues.BITSHIFTS_RIGHT_BLOCK_TO_REGION;}
-    public int regionY() {return _blockY >> MagicValues.BITSHIFTS_RIGHT_BLOCK_TO_REGION;}
+    public int regionY() {return _blockY;}
     public int regionZ() {return _blockZ >> MagicValues.BITSHIFTS_RIGHT_BLOCK_TO_REGION;}
+    public int chunkRelativeBlockX() { return (blockX() < 0 ? -blockX() : blockX()) % MagicValues.HORIZONTAL_BLOCKS_IN_CHUNK; }
+    public int chunkRelativeBlockY() { return blockY(); }
+    public int chunkRelativeBlockZ() { return (blockZ() < 0 ? -blockZ() : blockZ()) % MagicValues.HORIZONTAL_BLOCKS_IN_CHUNK; }
+    public int regionRelativeChunkX() { return (chunkX() < 0 ? -chunkX() : chunkX()) % MagicValues.HORIZONTAL_CHUNKS_IN_REGION; }
+    public int regionRelativeChunkY() { return chunkY(); }
+    public int regionRelativeChunkZ() { return (chunkZ() < 0 ? -chunkZ() : chunkZ()) % MagicValues.HORIZONTAL_CHUNKS_IN_REGION; }
+
     public PrecisionLevel getPrecisionLevel() {
         return _level;
     }
