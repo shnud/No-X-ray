ae8969fc022801157cf8e9129d4924eb5586293a
shnud <andrewshand94@gmail.com>
2014-01-10 07:55:58 +0000

    Renamed to stop confusion with Bukkit blocks

---
+++ src/com/shnud/noxray/World/MapBlock.java
@@ -0,0 +1,43 @@
+package com.shnud.noxray.World;
+
+/**
+ * Created by Andrew on 09/01/2014.
+ */
+public class MapBlock {
+
+    private final byte _metadata;
+    private final int _blockID;
+    private final int _x, _y, _z;
+
+    public MapBlock(int blockID, int x, int y, int z) {
+        this(blockID, (byte) 0, x, y, z);
+    }
+
+    public MapBlock(int blockID, byte metadata, int x, int y, int z) {
+        _blockID = blockID;
+        _metadata = metadata;
+        _x = x;
+        _y = y;
+        _z = z;
+    }
+
+    public int getX() {
+        return _x;
+    }
+
+    public int getY() {
+        return _y;
+    }
+
+    public int getZ() {
+        return _z;
+    }
+
+    public byte getMetadata() {
+        return _metadata;
+    }
+
+    public int getBlockID() {
+        return _blockID;
+    }
+}
